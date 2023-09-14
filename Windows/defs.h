ca2865ce32b391807e62a6102bb60644828683ec
JackOfBlades232 <petrkiyashko232@gmail.com>
2023-09-14 20:27:43 +0300

    Init win-platform layer and reorganized folders

---
+++ Windows/defs.h
@@ -0,0 +1,54 @@
+/* RecklessPillager/Windows/defs.h */
+#include <stdint.h>
+
+typedef float     f32;
+typedef double    f64;
+typedef uint8_t   u8;
+typedef uint16_t  u16;
+typedef uint32_t  u32;
+typedef uint64_t  u64;
+typedef int8_t    s8;
+typedef int16_t   s16;
+typedef int32_t   s32;
+typedef int64_t   s64;
+
+typedef int       bool;
+#define true      1
+#define false     0
+
+#if defined(USE_ASSERTIONS) && USE_ASSERTIONS == 1
+
+  #include <stdio.h>
+  #include <errno.h>
+
+  #define _LOG_STRERR_GEN(_func, _errno, _fmt, ...) fprintf(stderr, "[ERR] (%s:%d: errno: %s) " _fmt "\n", \
+          __FILE__, __LINE__, (errno == 0 ? "None" : _func(_errno)), ##__VA_ARGS__)
+  #define _LOG_STRERR(_func, _fmt, ...) _LOG_STRERR_GEN(_func, errno, _fmt, ##__VA_ARGS__)
+  #define _LOG_ERR(_fmt, ...) _LOG_STRERR(strerror, _fmt, ##__VA_ARGS__)
+  
+  #define ASSERT(_e) if(!(_e)) { fprintf(stderr, "Assertion (" #_e ") failed at %s:%d\n", __FILE__, __LINE__); exit(1); }
+  #define ASSERTF(_e, _fmt, ...) if(!(_e)) { fprintf(stderr, "(%s:%d) " _fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__); exit(1); }
+  #define ASSERT_ERR(_e) if(!(_e)) { _LOG_ERR("Assertion (" #_e ") failed at %s:%d\n", __FILE__, __LINE__); exit(1); }
+  #define ASSERTF_ERR(_e, _fmt, ...) if(!(_e)) { _LOG_ERR(_fmt, ##__VA_ARGS__); exit(1); }
+  #define ASSERT_LIBERR(_e, _libfunc) if(!(_e)) { _LOG_STRERR(_libfunc, "Assertion (" #_e ") failed at %s:%d", __FILE__, __LINE__); exit(1); }
+  #define ASSERTF_LIBERR(_e, _libfunc, _fmt, ...) if(!(_e)) { _LOG_STRERR(_libfunc, _fmt, ##__VA_ARGS__); exit(1); }
+  #define ASSERT_LIBERR_GEN(_e, _libfunc, _errno) if(!(_e)) { _LOG_STRERR_GEN(_libfunc, _errno, "Assertion (" #_e ") failed at %s:%d", __FILE__, __LINE__); exit(1); }
+  #define ASSERTF_LIBERR_GEN(_e, _libfunc, _errno, _fmt, ...) if(!(_e)) { _LOG_STRERR_GEN(_libfunc, _errno, _fmt, ##__VA_ARGS__); exit(1); }
+  
+  #define __ASSERT_GLUE(_a, _b) _a ## _b
+  #define _ASSERT_GLUE(_a, _b) __ASSERT_GLUE(_a, _b)
+  #define STATIC_ASSERT(_e) enum { _ASSERT_GLUE(s_assert_fail_, __LINE__) = 1 / (int)(!!(_e)) }
+
+#else
+
+  #define ASSERT(_e)
+  #define ASSERTF(_e, _fmt, ...)
+  #define ASSERT_ERR(_e)
+  #define ASSERTF_ERR(_e, _fmt, ...)
+  #define ASSERT_LIBERR(_e, _libfunc)
+  #define ASSERTF_LIBERR(_e, _libfunc, _fmt, ...)
+  #define ASSERT_LIBERR_GEN(_e, _libfunc, _error)
+  #define ASSERTF_LIBERR_GEN(_e, _libfunc, _error, _fmt, ...)
+  #define STATIC_ASSERT(_e)
+
+#endif
---
+++ Windows/win32_rp.c
@@ -0,0 +1,66 @@
+﻿/* RecklessPillager/Windows/win32_rp.c */
+#include "defs.h"
+
+#include <windows.h>
+
+#include <stdio.h>
+#include <stdarg.h>
+
+// Limited to 4kb
+void odprintf(const char* format, ...)
+{
+    char buf[4096];
+    char* p = buf;
+    va_list args;
+    int nchars;
+    const int max_len = sizeof(buf) - 3; // \r\n\0
+
+    va_start(args, format);
+    nchars = _vsnprintf_s(buf, sizeof(buf), max_len, format, args);
+    va_end(args);
+
+    if (nchars > 0)
+        p += nchars;
+
+    *(p++) = '\r';
+    *(p++) = '\n';
+    *p = '\0';
+
+    OutputDebugStringA(buf);
+}
+
+int APIENTRY wWinMain(_In_ HINSTANCE     h_instance,
+                      _In_opt_ HINSTANCE h_prev_instance,
+                      _In_ LPWSTR        lp_cmd_line,
+                      _In_ int           n_cmd_show)
+{
+    MSG msg = { 0 };
+
+    LARGE_INTEGER perf_count_freq_res;
+    QueryPerformanceFrequency(&perf_count_freq_res);
+    u64 perf_count_freq = perf_count_freq_res.QuadPart;
+
+    LARGE_INTEGER last_counter;
+    LARGE_INTEGER end_counter;
+    QueryPerformanceCounter(&last_counter);
+
+    while (msg.message != (WM_QUIT | WM_CLOSE))
+    {
+        if (PeekMessage(&msg, 0, 0, 0, PM_REMOVE))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+        else
+        {
+            QueryPerformanceCounter(&end_counter);
+            u64 dcounts = end_counter.QuadPart - last_counter.QuadPart;
+            f32 dt = (f32)dcounts / perf_count_freq;
+            last_counter = end_counter;
+
+            odprintf("%f ms/frame, %d fps\n", dt * 1e3, (u32)(1.0f / dt));
+        }
+    }
+
+    return (int)msg.wParam;
+}
