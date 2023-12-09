06bff571f12cb301ab620376a678ca159baf86dc
JackOfBlades232 <petrkiyashko232@gmail.com>
2023-12-09 14:21:34 +0300

    Collisions wip

--- TestGames/game_jailbreak.c
+++ TestGames/game_jailbreak.c
@@ -7,6 +7,9 @@
 #define MAX(_a, _b) ((_a) > (_b) ? (_a) : (_b))
 #define MIN(_a, _b) ((_a) < (_b) ? (_a) : (_b))
 
+#define ABS(_a) ((_a) < 0 ? -(_a) : (_a))
+#define SGN(_a) ((_a) < 0 ? -1 : ((_a) > 0 ? 1 : 0))
+
 #define EPS 0.0001f
 
 // @TODO: check out fps drops
@@ -28,6 +31,11 @@ typedef struct vec2f_tag {
     f32 x, y;
 } vec2f_t;
 
+typedef struct ray_tag {
+    vec2f_t orig;
+    vec2f_t dir;
+} ray_t;
+
 typedef union rect_tag {
     struct {
         vec2f_t pos;
@@ -38,6 +46,12 @@ typedef union rect_tag {
         f32 width, height;
     };
 } rect_t;
+
+static inline vec2f_t vec2f_lit(f32 x, f32 y)
+{
+    vec2f_t res = { x, y };
+    return res;
+}
 
 static inline vec2f_t vec2f_add(vec2f_t v1, vec2f_t v2)
 {
@@ -51,10 +65,48 @@ static inline vec2f_t vec2f_scale(vec2f_t v, f32 scale)
     return res;
 }
 
+static inline vec2f_t vec2f_neg(vec2f_t v)
+{
+    vec2f_t res = { -v.x, -v.y };
+    return res;
+}
+
+static inline vec2f_t vec2f_normalized(vec2f_t v)
+{
+    return vec2f_scale(v, 1.f / sqrt(v.x*v.x + v.y*v.y));
+}
+
+static inline void vec2f_translate(vec2f_t *v, vec2f_t amt)
+{
+    v->x += amt.x;
+    v->y += amt.y;
+}
+
 static inline bool rects_intersect(rect_t r1, rect_t r2)
 {
     return (r1.x <= r2.x + r2.width && r2.x <= r1.x + r1.width) &&
            (r1.y <= r2.y + r2.height && r2.y <= r1.y + r1.height);
+}
+
+static bool intersect_ray_with_rect(ray_t ray, rect_t rect, f32 *tmin_out, f32 *tmax_out)
+{
+    f32 tx1 = (rect.x - ray.orig.x) / ray.dir.x;
+    f32 tx2 = (rect.x + rect.width - ray.orig.x) / ray.dir.x;
+    f32 ty1 = (rect.y - ray.orig.y) / ray.dir.y;
+    f32 ty2 = (rect.y + rect.width - ray.orig.y) / ray.dir.y;
+
+    f32 tx_min = MIN(tx1, tx2);
+    f32 ty_min = MIN(ty1, ty2);
+    f32 tx_max = MAX(tx1, tx2);
+    f32 ty_max = MAX(ty1, ty2);
+
+    if (tx_max < ty_min || ty_max < tx_min)
+        return false;
+    else {
+        if (tmin_out) *tmin_out = MAX(tx_min, ty_min);
+        if (tmax_out) *tmax_out = MIN(tx_max, ty_max);
+        return true;
+    }
 }
 
 #define INCLUDE_TYPE(_type, _name) union { _type; _type _name; };
@@ -92,7 +144,7 @@ static inline void draw_body(offscreen_buffer_t *backbuffer, body_t *body)
 
 static inline void update_body(body_t *body, f32 dt)
 {
-    body->pos = vec2f_add(body->pos, vec2f_scale(body->vel, dt));
+    vec2f_translate(&body->pos, vec2f_scale(body->vel, dt));
 }
 
 // @TEMP
@@ -103,32 +155,32 @@ static void clamp_body(body_t* body, u32 world_w, u32 world_h, bool flip_vel_x, 
         if (body->vel.x < -EPS)
             body->vel.x = -body->vel.x;
         else if (body->vel.x < EPS)
-            body->vel.x = 1.0f;
+            body->vel.x = 1.f;
     } else if (body->x > world_w - body->width - EPS) {
         body->x = world_w - body->width - EPS;
         if (body->vel.x > EPS) {
             if (flip_vel_x)
                 body->vel.x = -body->vel.x;
             else
-                body->vel.x = 0.0f;
+                body->vel.x = 0.f;
         } else if (body->vel.x > -EPS)
-            body->vel.x = -1.0f;
+            body->vel.x = -1.f;
     }
     if (body->y < EPS) {
         body->y = EPS;
         if (body->vel.y < -EPS)
             body->vel.y = -body->vel.y;
         else if (body->vel.y < EPS)
-            body->vel.y = 1.0f;
+            body->vel.y = 1.f;
     } else if (body->y > world_h - body->height - EPS) {
         body->y = world_h - body->height - EPS;
         if (body->vel.y > EPS) {
             if (flip_vel_y)
                 body->vel.y = -body->vel.y;
             else
-                body->vel.y = 0.0f;
+                body->vel.y = 0.f;
         } else if (body->vel.y > -EPS)
-            body->vel.y = -1.0f;
+            body->vel.y = -1.f;
     }
 }
 
@@ -137,14 +189,15 @@ void game_init(input_state_t *input, offscreen_buffer_t *backbuffer, sound_buffe
     player.width  = 150;
     player.height = 30;
     player.x      = backbuffer->width/2 - player.width/2;
-    player.y      = 3*backbuffer->height/4 - player.height/2;
+    player.y      = 5*backbuffer->height/6 - player.height/2;
     player.col    = 0xFFFF0000;
 
     ball.width    = 15;
     ball.height   = 15;
     ball.x        = player.x + player.width/2 - ball.width/2;
     ball.y        = player.y - ball.height;
-    ball.vel.y    = 400.0;
+    ball.vel.y    = 300.0;
+    ball.vel.x    = 300.0;
     ball.col      = 0xFFFFFF00;
 }
 
@@ -160,11 +213,11 @@ void game_update_and_render(input_state_t *input, offscreen_buffer_t *backbuffer
     if (input_key_is_down(input, INPUT_KEY_ESC))
         input->quit = true;
     
-    player.vel.x = 0.0f;
+    player.vel.x = 0.f;
     if (input_char_is_down(input, 'A'))
-        player.vel.x -= 200.0f;
+        player.vel.x -= 200.f;
     if (input_char_is_down(input, 'D'))
-        player.vel.x += 200.0f;
+        player.vel.x += 200.f;
 
     update_body(&player, dt);
     update_body(&ball, dt);
@@ -174,19 +227,37 @@ void game_update_and_render(input_state_t *input, offscreen_buffer_t *backbuffer
 
     // @TEST: noodling around, this is not correct
     if (rects_intersect(player.r, ball.r)) {
-        if (ball.y < player.y + player.height && ball.y > player.y + player.height*0.5f - ball.height*0.5f) {
-            ball.y = player.y + player.height;
-            if (ball.vel.y < -EPS)
-                ball.vel.y = -ball.vel.y;
-            else if (ball.vel.y < player.vel.y + EPS)
-                ball.vel.y = player.vel.y + EPS;
-        } else if (ball.y > player.y - ball.height && ball.y <= player.y + player.height*0.5f - ball.height*0.5f) {
-            ball.y = player.y - ball.height;
-            if (ball.vel.y > EPS)
-                ball.vel.y = -ball.vel.y;
-            else if (ball.vel.y > player.vel.y - EPS)
-                ball.vel.y = player.vel.y - EPS;
+        rect_t ext_player_rect = { player.x - ball.width*0.5f,
+                                   player.y - ball.height*0.5f,
+                                   player.width + ball.width,
+                                   player.height + ball.height };
+
+        ray_t vel_ray = { vec2f_add(ball.pos, vec2f_scale(ball.size, 0.5f)),
+                          vec2f_normalized(ball.vel) };
+
+        f32 tmin = 0.f;
+        ASSERTF(intersect_ray_with_rect(vel_ray, ext_player_rect, &tmin, NULL),
+                "BUG: Velocity ray must intersect with extended rect\n");
+
+        vec2f_translate(&ball.pos, vec2f_scale(vel_ray.dir, tmin));
+
+        if (ball.x >= player.x - ball.width - EPS && 
+            ball.x <= player.x + player.width + EPS)
+        {
+            ball.vel.y = -ball.vel.y;
         }
+        if (ball.y >= player.y - ball.height - EPS && 
+            ball.y <= player.y + player.height + EPS)
+        {
+            // @TODO: maybe use momentum balance?
+            ball.vel.x = -ball.vel.x;
+            if (SGN(ball.vel.x) == SGN(player.vel.x) &&
+                ABS(ball.vel.x) < ABS(player.vel.x) + EPS)
+            {
+                ball.vel.x = SGN(ball.vel.x) * (ABS(player.vel.x) + EPS);
+            }
+        }
+
     }
 
     clamp_body(&ball, backbuffer->width, backbuffer->height, true, true);
@@ -201,11 +272,12 @@ void game_redraw(offscreen_buffer_t *backbuffer)
 
     // @TODO: temp solution, need prev screen size to reposition and resize player
     player.x     = backbuffer->width/2 - player.width/2;
-    player.y     = 3*backbuffer->height/4 - player.height/2;
+    player.y     = 5*backbuffer->height/6 - player.height/2;
     player.vel.x = 0;
     ball.x       = player.x + player.width/2 - ball.width/2;
     ball.y       = player.y - ball.height;
-    ball.vel.y   = 400.0;
+    ball.vel.y   = 300.0;
+    ball.vel.x   = 300.0;
 
     draw_body(backbuffer, &player);
     draw_body(backbuffer, &ball);
