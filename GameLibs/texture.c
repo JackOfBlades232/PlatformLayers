4480d7fd17b5ac7a1aaf6efc26ba55233616735c
JackOfBlades232 <petrkiyashko232@gmail.com>
2024-01-20 23:14:39 +0200

    Small opt: removed floor from tex_get_pixel, fps 7-8->13-14

--- GameLibs/texture.c
+++ GameLibs/texture.c
@@ -22,8 +22,9 @@ u32 texture_get_pixel(texture_t *tex, vec2f_t dst_coord, vec2f_t dst_dim)
     vec2f_t tex_dim = { tex->width, tex->height };
     vec2f_t texcoord = vec2f_mul(uv, tex_dim);
 
-    u32 x = CLAMP(floor(texcoord.x), 0, tex->width-1);
-    u32 y = CLAMP(floor(texcoord.y), 0, tex->height-1);
+    // @TODO: fast floor for correct reads
+    u32 x = CLAMP(texcoord.x, 0, tex->width-1);
+    u32 y = CLAMP(texcoord.y, 0, tex->height-1);
 
     if (tex->type == textype_grayscale) {
         u8 scale = ((u8 *)tex->mem)[y*tex->width + x];
